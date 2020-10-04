456a16905a0c3a78a013362eb92ee29511db0e38
AJ Catambay <ajcatambay@gmail.com>
2020-10-04 21:44:51 +0800

    publish-consume demo using DefaultJmsListenerContainerFactory

--- src/main/java/com/bridgingcode/springbootactivemqdemo/config/JmsConfig.java
+++ src/main/java/com/bridgingcode/springbootactivemqdemo/config/JmsConfig.java
@@ -1,17 +1,25 @@
 package com.bridgingcode.springbootactivemqdemo.config;
 
-import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
-import javax.jms.Queue;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+
+import javax.jms.ConnectionFactory;
 
 @Configuration
 @EnableJms
 public class JmsConfig {
 
     @Bean
-    public Queue queue() {
-        return new ActiveMQQueue("bridgingcode-queue");
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory) {
+
+        DefaultJmsListenerContainerFactory factory
+                = new DefaultJmsListenerContainerFactory();
+
+        factory.setConnectionFactory(connectionFactory);
+        factory.setConcurrency("5-10");
+
+        return factory;
     }
 }
--- src/main/java/com/bridgingcode/springbootactivemqdemo/publisher/controller/PublishController.java
+++ src/main/java/com/bridgingcode/springbootactivemqdemo/publisher/controller/PublishController.java
@@ -5,18 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.jms.Queue;
-
 @RestController
 public class PublishController {
-
-    @Autowired
-    private Queue queue;
 
     @Autowired
     private JmsTemplate jmsTemplate;
@@ -24,7 +18,7 @@ public class PublishController {
     @PostMapping("/publishMessage")
     public ResponseEntity<String> publishMessage(@RequestBody SystemMessage systemMessage) {
         try {
-            jmsTemplate.convertAndSend(queue, systemMessage);
+            jmsTemplate.convertAndSend("bridgingcode-queue", systemMessage);
 
             return new ResponseEntity<>("Sent.", HttpStatus.OK);
 
